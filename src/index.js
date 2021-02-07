606e623aad5ace445a8a90f796d854ad59fed6e9
Illia Ilin <sunrise.th13@gmail.com>
2021-02-07 19:38:31 +0200

    Animated mobile menu added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import './style.scss';
 const header = document.querySelector('.header');
-const headerMenu = document.querySelector('.header_menu');
+const headerMenu = document.querySelector('.header_toggle');
 const overlay = document.querySelector('.overlay');
 headerMenu.addEventListener('click', () => {
   if (header.classList.contains('open')) {
     overlay.classList.remove('fade-in');
     overlay.classList.add('fade-out');
+    document.body.classList.remove('noscroll');
   } else {
     overlay.classList.add('fade-in');
     overlay.classList.remove('fade-out');
+    document.body.classList.add('noscroll');
   }
   header.classList.toggle('open');
 });
@@ -16,4 +18,5 @@ overlay.addEventListener('click', () => {
   header.classList.toggle('open');
   overlay.classList.remove('fade-in');
   overlay.classList.add('fade-out');
+  document.body.classList.remove('noscroll');
 });
