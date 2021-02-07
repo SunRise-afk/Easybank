187157ac43a92a6c4511aa8a41f4ad51087bf9dc
Illia Ilin <sunrise.th13@gmail.com>
2021-02-07 18:47:42 +0200

    Animated burdger menu and overlay displaying added

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
-import "./style.scss";
-
+import './style.scss';
+const header = document.querySelector('.header');
+const headerMenu = document.querySelector('.header_menu');
+const overlay = document.querySelector('.overlay');
+headerMenu.addEventListener('click', () => {
+  if (header.classList.contains('open')) {
+    overlay.classList.remove('fade-in');
+    overlay.classList.add('fade-out');
+  } else {
+    overlay.classList.add('fade-in');
+    overlay.classList.remove('fade-out');
+  }
+  header.classList.toggle('open');
+});
+overlay.addEventListener('click', () => {
+  header.classList.toggle('open');
+  overlay.classList.remove('fade-in');
+  overlay.classList.add('fade-out');
+});
