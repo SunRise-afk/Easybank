2ba22f7207cf683631728819bf7b6eae72773ba1
Illia Ilin <sunrise.th13@gmail.com>
2021-02-07 16:36:39 +0200

    Adding svg in dependencies list of file loader, adding color variables

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,20 @@
-const path = require("path");
-const HTMLWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  context: path.resolve(__dirname, "src"),
-  entry: "/index.js",
+  mode: 'development',
+  context: path.resolve(__dirname, 'src'),
+  entry: '/index.js',
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    publicPath: "",
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '',
   },
   plugins: [
     new HTMLWebpackPlugin({
-      template: "./index.html",
+      template: './index.html',
     }),
     new MiniCssExtractPlugin(),
     new CleanWebpackPlugin(),
@@ -24,24 +24,24 @@ module.exports = {
       {
         test: /\.(html)$/,
         use: {
-          loader: "html-loader",
+          loader: 'html-loader',
         },
       },
       {
         test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
       {
         test: /\.scss$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
       {
-        test: /\.(jpg|png|gif)$/i,
-        use: ["file-loader"],
+        test: /\.(jpg|png|gif|svg)$/i,
+        use: ['file-loader'],
       },
       {
         test: /\.(ttf|woff|woff2|eot)$/i,
-        use: ["file-loader"],
+        use: ['file-loader'],
       },
     ],
   },
